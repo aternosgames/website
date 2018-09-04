99a17a7b4b207eb018ad0920969f3741810b6b57
Väinämö Łūmikērø <vainamo@git.festival.ml>
2018-09-05 01:24:37 +0200

    Final Cleanup + Preparations

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,68 @@
 <template>
-    <div class="home">
-        <img alt="Vue logo" src="../assets/logo.png">
-        <hello-world msg="Aternos" />
-    </div>
+
+    <ag-container expand-vertically="true">
+        <section class="hero-content">
+            <img src="../assets/logotype-horizontal-light.png" alt="Aternos" width="200" />
+
+            <div>
+                <h1>Aternos Games</h1>
+                <h2>An <em>open-source</em>, <em>community-driven</em> and <em>ad-supported</em> Minecraft 1.13 network. Made by Aternos.</h2>
+            </div>
+
+            <p>&copy; 2018 Aternos</p>
+        </section>
+    </ag-container>
+
 </template>
 
 <script lang="ts">
 
     import { Component, Vue } from 'vue-property-decorator';
-    import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+    
+    import AgContainer from '@/components/AgContainer.vue';
 
     @Component({
         components: {
-            HelloWorld,
+            AgContainer
         },
     })
     export default class Home extends Vue {}
 
 </script>
+
+<style lang="scss" scoped>
+
+    @import "../style/_breakpoints";
+
+    section.hero-content {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        min-height: calc(100vh - 24px);
+
+        @include for-desktop-up {
+            min-height: calc(100vh - 48px);
+        }
+
+        h1 {
+            color: #F0F0F0;
+            font-size: 7vw;
+            font-weight: 600;
+
+            @include for-desktop-up {
+                font-size: 3vw;
+            }
+        }
+
+        h2 {
+            color: #F0F0F0;
+            font-size: 3vw;
+            font-weight: 500;
+
+            @include for-desktop-up {
+                font-size: 1vw;
+            }
+        }
+    }
+
+</style>
